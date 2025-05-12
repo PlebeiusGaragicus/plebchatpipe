5904ecad324bc70d3803147981011e2e687cd655
PlebbyG <plebeiusgaragicus@gmail.com>
2025-05-12 15:02:25 -0700

    tidy done

--- src/graphs/echobot/graph.py
+++ src/graphs/echobot/graph.py
@@ -1,47 +1,31 @@
 """
 This is an example LangGraph agent with the most basic functionality.
 
-It serves as an example.
-
+It serves as an example
 """
-
-#TODO: graph.get_graph().draw_mermaid_png())
-
-
 
 ##############################################################
 ### DEFINE GRAPH STATE
 ##############################################################
-import os
-import json
 import operator
+from typing import Annotated
 from pydantic import BaseModel, Field
-from typing import Optional, Any, Annotated
-
-from langchain_core.runnables import RunnableConfig
 
 
-##############################################################
 class State(BaseModel):
     messages: Annotated[list, operator.add] = Field(default_factory=list)
 
 
-
-
-
-
-
 ##############################################################
-### DEFINE OUR NODES
+### DEFINE GRAPH NODES
 ##############################################################
+from langchain_core.runnables import RunnableConfig
 from langgraph.types import StreamWriter
 
-# These are my custom enhancements
-from ..common import NodeOutputType, think, answer, think_codeblock
 from graphs import configuration
+from graphs.common import NodeOutputType, think, answer, think_codeblock
 
-##############################################################
-#NOTE: since we aren't using an LLM to generate tokens, we need to use the writer to print to the UI
+
 def echo(state: State, config: RunnableConfig, writer: StreamWriter):
     configurable = configuration.Configuration.from_runnable_config(config)
 
@@ -49,6 +33,7 @@ def echo(state: State, config: RunnableConfig, writer: StreamWriter):
         think_codeblock( configurable.__dict__, writer=writer )
         think( "Geesh... this guy's an idiot amirite?", writer=writer )
 
+    #NOTE: since we aren't using an LLM to generate tokens, we need to use the writer to print to the UI
     echoback = state.messages[-1]['content']
     answer( echoback, writer=writer )
 
@@ -60,7 +45,6 @@ def echo(state: State, config: RunnableConfig, writer: StreamWriter):
 from langgraph.graph import StateGraph, START, END
 
 
-##############################################################
 graph_builder = StateGraph(State, input=State, config_schema=configuration.Configuration)
 
 graph_builder.add_node("echo", echo, metadata={"node_output_type": NodeOutputType.ANSWER})
